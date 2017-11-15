f3451b6c5013cc212913c132459749dd4594931b
Rob Jones <robertjones>
2017-11-15 09:04:51 +0000

    fix webpack build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,13 @@
 const babelOptions = require('../babelrc');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 
 export default {
   context: process.cwd(),
   entry: {
-    main: '../src/client/index.js'
+    main: ['./src/client/index.js']
   },
   output: {
-    path: path.resolve(process.cwd(), 'build', 'client'),
+    path: path.resolve('build', 'client'),
     filename: '[name].js',
   },
   resolve: {
