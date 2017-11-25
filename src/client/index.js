b1a27183b53c2dca9276eaf0d836d094ffa09f2e
Rob Jones <robertjones>
2017-11-25 20:16:20 +0000

    scss utils and breakpoints mixin

--- src/client/index.js
+++ src/client/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom";
 
 import Component from "./component";
 
+import breakpoints from "./utils/breakpoints";
+
 ReactDOM.render(
 	<div>
 		<Component />
+		{`I can pass scss values to the page: ${breakpoints.mobile}`}
 	</div>,
 	document.getElementById("App")
 );
